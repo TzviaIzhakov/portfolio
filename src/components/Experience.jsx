9fad224408163c3f1b53ec856f32f1d48fb5c22f
TzviaIzhakov <tzvia.izhakov@gmail.com>
2024-03-10 16:21:01 +0200

    cleaning code

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,8 +1,10 @@
+/* eslint-disable react/prop-types */
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
-import { motion } from 'framer-motion';
+
 import experiences from '../demo-data/experience';
 
 import 'react-vertical-timeline-component/style.min.css';
+
 function ExperienceCard({ experience }) {
 	return (
 		<section className='timeline-container'>
@@ -12,19 +14,19 @@ function ExperienceCard({ experience }) {
 					color: '#fff',
 				}}
 				contentArrowStyle={{ borderRight: '7px solid  #232631' }}
-				date={experience.date}
-				iconStyle={{ background: experience.iconBg }}
+				date={experience?.date}
+				iconStyle={{ background: experience?.iconBg }}
 				icon={
 					<div>
-						<img className='experience-icon' src={experience.icon} alt={experience.companyName} />
+						<img className='experience-icon' src={experience?.icon} alt={experience?.companyName} />
 					</div>
 				}
 			>
 				<div className='experience-details'>
-					<h3 className='experience-title'>{experience.title}</h3>
-					<p className='experience-company-name'>{experience.companyName}</p>
+					<h3 className='experience-title'>{experience?.title}</h3>
+					<p className='experience-company-name'>{experience?.companyName}</p>
 					<ul className='experience-points'>
-						{experience.points.map((p, index) => (
+						{experience?.points?.map((p, index) => (
 							<li key={`experience-point-${index}`} className='experience-point'>
 								{p}
 							</li>
@@ -35,6 +37,7 @@ function ExperienceCard({ experience }) {
 		</section>
 	);
 }
+
 export function Experience() {
 	return (
 		<section className='experience-container main-layout'>
