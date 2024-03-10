aed44d2f6dc803a0c32851657f17378effa1252d
TzviaIzhakov <tzvia.izhakov@gmail.com>
2024-03-10 16:22:18 +0200

    arranging imports and cleaning code

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,16 +1,16 @@
-import technologies from '../demo-data/technologies';
 import { BallCanvas } from './canvas/BallCanvas';
+
+import technologies from '../demo-data/technologies';
+
 export function Tech() {
 	return (
 		<section className='technologies-container main-layout'>
 			<div className=' flex wrap justify-center technologies'>
-				{technologies.map((technology) => {
-					return (
-						<div className='technology flex justify-center' key={technologies.name}>
-							<BallCanvas icon={technology.icon} />
-						</div>
-					);
-				})}
+				{technologies.map((technology) => (
+					<div className='technology flex justify-center' key={technologies.name}>
+						<BallCanvas icon={technology.icon} />
+					</div>
+				))}
 			</div>
 		</section>
 	);
