61d0a355b58c201ae442717e361afe8bd158bcc6
TzviaIzhakov <tzvia.izhakov@gmail.com>
2024-03-10 16:20:26 +0200

    cleaning code

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,18 @@
 import { ComputerCanvas } from './canvas/Computers';
 import { motion } from 'framer-motion';
 
-// import { motion } from 'framer-motion';
 export function Hero() {
 	return (
 		<section className='hero-wrapper'>
 			<div className='main-layout'>
-				{/* <div className='dot'></div>
-				<div className='linear-line'></div> */}
 				<div className='typewriter'>
 					<h1 className='introduction-header'>
-						Hi, I'm <span className='firstname'>Tzvia</span>
+						{"Hi, I'm"} <span className='first-name'>Tzvia</span>
 					</h1>
 				</div>
+
 				<p className='introduction-content'>I Develop FullStack applications that blend functionality with a seamless user experience.</p>
+
 				<div style={{ width: '100%', margin: '0 auto', height: '400px', position: 'relative' }} className='computer-wrapper'>
 					<ComputerCanvas />
 					<a href='#about'>
