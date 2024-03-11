f6580518fa5a461ae3d8f627d659e8de267755aa
TzviaIzhakov <tzvia.izhakov@gmail.com>
2024-03-11 11:37:54 +0200

    project demo data

---
+++ src/demo-data/projects.js
@@ -0,0 +1,26 @@
+import sixerrImg from '../assets/img/projects/sixerr.png';
+
+const projects = [
+	{
+		name: 'Sixerr',
+		description: 'A dynamic marketplace application that unites freelancers with clients in search of top-notch creative services.',
+		tags: [
+			{
+				name: 'react',
+				color: 'blue-text-gradient',
+			},
+			{
+				name: 'mongodb',
+				color: 'green-text-gradient',
+			},
+			{
+				name: 'sass',
+				color: 'pink-text-gradient',
+			},
+		],
+		image: sixerrImg,
+		source_code_link: 'https://github.com/razamsalem/Sixerr',
+	},
+];
+
+export default projects;
