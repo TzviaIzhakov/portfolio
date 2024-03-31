1e7b0eac0fcffd9a1c37db62bb645a2fedf17d30
TzviaIzhakov <tzvia.izhakov@gmail.com>
2024-03-31 12:35:13 +0300

    no need of using flex in title

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,15 +5,11 @@ import { ProjectCard } from './ProjectCard';
 export function Projects() {
 	return (
 		<section className='project-container main-layout'>
-			<div className='flex column'>
-				<span className='project-title-main'>My Work</span>
-				<h1 className='project-sub-tile'>Projects.</h1>
-			</div>
-			<div>
-				{projects.map((project, index) => (
-					<ProjectCard key={`project-${index}`} index={index} {...project} />
-				))}
-			</div>
+			<span className='project-title-main'>My Work</span>
+			<h1 className='project-sub-tile'>Projects.</h1>
+			{projects.map((project, index) => (
+				<ProjectCard key={`project-${index}`} index={index} {...project} />
+			))}
 		</section>
 	);
 }
