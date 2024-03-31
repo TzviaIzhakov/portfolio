35659adb3f2cfe96f15d1cea4e49228af9547452
TzviaIzhakov <tzvia.izhakov@gmail.com>
2024-03-31 12:27:11 +0300

    there is no need to use Router, deleting this

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,8 @@
 import ReactDOM from 'react-dom/client';
-import { Router } from 'react-router-dom';
 
 import App from './App.jsx';
 
 import './assets/styles/main.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-	// <Router>
-	<App />
-	// </Router>
-);
+root.render(<App />);
