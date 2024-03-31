2944c3360b3e9f4c170bac0403b53f8136c240c4
TzviaIzhakov <tzvia.izhakov@gmail.com>
2024-03-31 12:28:25 +0300

    creating a new logic of context and custom hooks in the header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
-import { Navbar } from './components/Navbar';
-import { Hero } from './components/Hero';
-import { About } from './components/About';
-import { Experience } from './components/Experience';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import useHeader from './custom-hooks/useHeader';
+import HeaderContext from './contexts/HeaderContext';
+
+import Hero from './components/Hero';
+import About from './components/About';
 import { Tech } from './components/Tech';
+import Contact from './components/Contact';
+import { Navbar } from './components/Navbar';
 import { Projects } from './components/Projects';
+import Experience from './components/Experience';
 
 function App() {
+	const headerBackground = useHeader();
+
 	return (
-		<main className='bg-img'>
-			<Navbar />
-			<Hero />
-			<About />
-			<Experience />
-			<Tech />
-			<Projects />
-		</main>
+		<Router>
+			<main className='app'>
+				<HeaderContext.Provider value={headerBackground}>
+					<Navbar />
+					<Hero />
+				</HeaderContext.Provider>
+				<About />
+				<Experience />
+				<Tech />
+				<Contact />
+				<Projects />
+			</main>
+		</Router>
 	);
 }
 
