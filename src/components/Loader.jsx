60f57f46b898f9530aa51f9020dc4668882ec2ba
TzviaIzhakov <tzvia.izhakov@gmail.com>
2024-03-31 12:31:08 +0300

    creating my own loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,10 +1,7 @@
-import { Html, useProgress } from '@react-three/drei';
-
 export function Loader() {
-	const { progress } = useProgress();
 	return (
-		<span className='canvas-loader-container'>
-			<p className='canvas-loader'>{progress.toFixed(2)}%</p>
-		</span>
+		<div className='flex justify-center'>
+			<span className='loader'></span>
+		</div>
 	);
 }
