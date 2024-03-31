a8af64206137a88ca7c3df9d0a5d3f8b9302f64c
TzviaIzhakov <tzvia.izhakov@gmail.com>
2024-03-31 12:35:50 +0300

    changing the key name into  technology.name

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -7,7 +7,7 @@ export function Tech() {
 		<section className='technologies-container main-layout'>
 			<div className=' flex wrap justify-center technologies'>
 				{technologies.map((technology) => (
-					<div className='technology flex justify-center' key={technologies.name}>
+					<div className='technology flex justify-center' key={technology.name}>
 						<BallCanvas icon={technology.icon} />
 					</div>
 				))}
