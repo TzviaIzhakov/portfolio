4a441e81649345571088495d147e8e22472b2414
TzviaIzhakov <tzvia.izhakov@gmail.com>
2024-04-07 15:26:15 +0300

    changing the width of the canvas to fit in the grid

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -6,7 +6,7 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 function Earth() {
 	const earth = useGLTF('./planet/scene.gltf');
 
-	return <primitive object={earth.scene} scale={3.5} position-y={0} rotation-y={0} />;
+	return <primitive object={earth.scene} scale={2} position-y={0} rotation-y={0} />;
 }
 
 export function EarthCanvas() {
@@ -23,6 +23,7 @@ export function EarthCanvas() {
 					far: 200,
 					position: [-4, 3, 6],
 				}}
+				style={{ width: '98%', height: '100%' }}
 			>
 				<OrbitControls autoRotate enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
 				<Earth />
